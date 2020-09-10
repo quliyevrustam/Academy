acc9b2c829cbdb887ce0bdbc43045754c29b3522
Rustam <quliyev.rustam@gmail.com>
2020-09-10 13:23:46 +0000

    Use CustomerRepositoryInterface for customer email #6

--- app/code/Bulbulatory/Recommendations/Ui/Component/Listing/Column/Action.php
+++ app/code/Bulbulatory/Recommendations/Ui/Component/Listing/Column/Action.php
@@ -3,38 +3,46 @@ namespace Bulbulatory\Recommendations\Ui\Component\Listing\Column;
 
 use Magento\Customer\Model\CustomerFactory;
 use Magento\Framework\Escaper;
+use Magento\Framework\Exception\NoSuchEntityException;
 use Magento\Framework\View\Element\UiComponent\ContextInterface;
 use Magento\Framework\View\Element\UiComponentFactory;
 use Magento\Ui\Component\Listing\Columns\Column;
+use Magento\Customer\Api\CustomerRepositoryInterface as CustomerRepository;
 
 class Action extends Column
 {
     protected $escaper;
 
-    protected $customerLoader;
+    protected $customerRepository;
 
     public function __construct(
         ContextInterface $context,
         UiComponentFactory $uiComponentFactory,
-        CustomerFactory $customerLoader,
+        CustomerRepository $customerRepository,
         Escaper $escaper,
         array $components = [],
         array $data = []
     ) {
         $this->escaper = $escaper;
-        $this->customerLoader = $customerLoader;
+        $this->customerRepository = $customerRepository;
+
         parent::__construct($context, $uiComponentFactory, $components, $data);
     }
-    
+
     public function prepareDataSource(array $dataSource)
     {
         if (isset($dataSource['data']['items']))
         {
             foreach ($dataSource['data']['items'] as & $item)
             {
-                $customer = $this->customerLoader->create()->load((int)$item[$this->getData('name')]);
-
-                $item[$this->getData('name')] =$customer->getEmail();
+                try {
+                    $customer = $this->customerRepository->getById($item["customer_id"]);
+                    $item[$this->getData('name')] = $customer->getEmail();
+                }
+                catch(NoSuchEntityException $e)
+                {
+                    $item[$this->getData('name')] = 'Customer does not exist';
+                }
             }
         }
 
